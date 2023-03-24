598ac885faca82849619176447b9f620f7aa4c04
nownabe <nownabe@users.noreply.github.com>
2023-03-24 23:40:40 +0900

    Chapter 2

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/nownabe/monkey-language-go/lexer"
-	"github.com/nownabe/monkey-language-go/token"
+	"github.com/nownabe/monkey-language-go/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +23,20 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
