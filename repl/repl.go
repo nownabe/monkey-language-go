96dfeb25addcea1216c1453f730fb4ddb936c820
nownabe <nownabe@users.noreply.github.com>
2023-03-25 21:15:45 +0900

    3.5

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/nownabe/monkey-language-go/evaluator"
 	"github.com/nownabe/monkey-language-go/lexer"
 	"github.com/nownabe/monkey-language-go/parser"
 )
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
